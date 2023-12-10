ce3bf7bc39b35b8b9907a0b3966d773ae1b8049b
gonza56d <gonza56d@gmail.com>
2023-12-10 00:11:59 -0300

    Guitar model

--- app/core/models/enums.py
+++ app/core/models/enums.py
@@ -31,6 +31,18 @@ class PickupType(str, Enum):
     HOT_RAIL = 'Hot Rail Humbucker'
 
 
+class PickupMagnetType(str, Enum):
+    """Options for different types of pickup magnets."""
+
+    ALNICO_1 = 'Alnico I'
+    ALNICO_2 = 'Alnico II'
+    ALNICO_3 = 'Alnico III'
+    ALNICO_4 = 'Alnico IV'
+    ALNICO_5 = 'Alnico V'
+    ALNICO_8 = 'Alnico VIII'
+    CERAMIC = 'Ceramic'
+
+
 class NutType(str, Enum):
     """Options for different types of nuts."""
 
@@ -58,3 +70,63 @@ class ControlType(str, Enum):
     TONE_KNOB_PUSH_PULL = 'Tone knob w/ push pull'
     TONE_KNOB_PUSH_PUSH = 'Tone knob w/ push push'
     ROTARY_SWITCH = 'Rotary switch'
+
+
+class WoodType(str, Enum):
+    """Options for different types of woods."""
+
+    ALDER = 'Alder'
+    ASH = 'Ash'
+    BUBINGA = 'Bubinga'
+    WENGE = 'Wenge'
+    FLAMED_ASH = 'Flamed Ash'
+    BASSWOOD = 'Basswood'
+    MAHOGANY = 'Mahogany'
+    MAPLE = 'Maple'
+    FLAMED_MAPLE = 'Flamed Maple'
+    QUILTED_MAPLE = 'Quilted Maple'
+    ROASTED_MAPLE = 'Roasted Maple'
+    ROSEWOOD = 'Rosewood'
+    EBONY = 'Ebony'
+    OTHER = 'Other'
+
+
+class HeadstockType(str, Enum):
+    """Options for different types of headstocks."""
+
+    INLINE = 'Inline'
+    THREE_X_THREE = '3x3'
+    TWO_X_FOUR = '2x4'
+    THREE_X_FOUR = '3x4'
+    FOUR_X_FOUR = '4x4'
+    HEADLESS = 'Headless'
+    OTHER = 'Other'
+
+
+class FretType(str, Enum):
+    """Options for different types of headstocks."""
+
+    FRETLESS = 'Fretless'
+    VINTAGE = 'Vintage'
+    MEDIUM_JUMBO = 'Medium Jumbo'
+    JUMBO = 'Jumbo'
+    X_JUMBO = 'Extra Jumbo'
+
+
+class FretMaterialType(str, Enum):
+    """Options for different types of headstocks."""
+
+    BRASS = 'Brass'
+    NICKEL = 'Nickel'
+    HARD_NICKEL = 'Hard Nickel'
+    STAINLESS_STEEL = 'Stainless Steel'
+
+
+class ConstructionType(str, Enum):
+    """Options for different types of construction methods."""
+
+    BOLT_ON = 'Bolt On'
+    SET_NECK = 'Set Neck'
+    NECK_THRU = 'Neck Thru'
+    SET_THRU = 'Set Thru'
+    ONE_PIECE = 'One piece'
--- app/core/models/guitars.py
+++ app/core/models/guitars.py
@@ -1,5 +1,154 @@
 from dataclasses import dataclass
-from enum import Enum
+from uuid import UUID
+
+from .enums import *
 
 
+@dataclass(kw_only=True)
+class Color:
 
+    id: UUID
+    name: str
+
+
+@dataclass(kw_only=True)
+class Brand:
+
+    id: UUID
+    name: str
+    founded_in: int | None = None
+
+
+@dataclass(kw_only=True)
+class BrandedComponent:
+
+    id: UUID
+    brand: Brand
+    name: str
+    year_of_introduction: int | None = None
+    color: Color | None = None
+
+
+@dataclass(kw_only=True)
+class Bridge(BrandedComponent):
+
+    type: BridgeType
+
+
+@dataclass(kw_only=True)
+class Tuner(BrandedComponent):
+
+    type: TunerType
+
+
+@dataclass(kw_only=True)
+class Pickup(BrandedComponent):
+
+    type: PickupType
+    magnet_type: PickupMagnetType
+    is_active: bool
+    is_covered: bool = False
+
+
+@dataclass(kw_only=True)
+class Body(BrandedComponent):
+
+    core_wood: WoodType
+    top_wood: WoodType
+    back_wood: WoodType
+    is_chambered: bool
+    is_carved: bool
+    binding_front: Color | None = None
+    binding_back: Color | None = None
+
+
+@dataclass(kw_only=True)
+class Neck(BrandedComponent):
+
+    woods: list[WoodType]
+    pieces: int
+    fretboard: WoodType | None = None
+
+    @property
+    def description(self) -> str:
+        woods = self._get_woods_str()
+        pieces_word = 'piece' if self.pieces == 1 else 'pieces'
+        return f'{self.pieces} {pieces_word} neck of {woods.lower()}.'
+
+    def _get_woods_str(self) -> str:
+        woods_str = (
+            ' and '.join(wood for wood in self.woods)
+            if len(self.woods) == 2 else
+            ', '.join(wood for wood in self.woods)
+        )
+        return self._get_fixed_comma(woods_str)
+
+    def _get_fixed_comma(self, woods) -> str:
+        if len(self.woods) > 2:
+            last_comma_at = woods.rfind(',')
+            woods = f'{woods[:last_comma_at]} and{woods[last_comma_at + 1:]}'
+        return woods
+
+
+@dataclass(kw_only=True)
+class Guitar(BrandedComponent):
+
+    body: Body
+    neck: Neck
+    construction_method: ConstructionType
+    number_of_strings: int = 6
+    nut_type: NutType
+    is_nut_compensated: bool
+    number_of_frets: int
+    frets_type: FretType
+    frets_material: FretMaterialType | None = None
+    headstock_type: HeadstockType
+    tuners: Tuner
+    bridge: Bridge
+    bridge_pickup: Pickup | None = None
+    middle_pickup: Pickup | None = None
+    neck_pickup: Pickup | None = None
+    has_piezo: bool = False
+    controls: list[ControlType]
+
+    @property
+    def color(self) -> Color:
+        """Overriden. Returns the color of the body of the guitar."""
+        return self.body.color
+
+    @property
+    def hardware_color_str(self) -> str:
+        """
+        Get readable message for the hardware color(s).
+
+        Example: 'Gold, Black.'
+        """
+        return ', '.join(
+            component.color.name for component in self.hardware
+        )
+
+    @property
+    def hardware(self) -> list[BrandedComponent]:
+        return [self.bridge, self.tuners]
+
+    @property
+    def controls_str(self) -> str:
+        """
+        Get readable message for the list of controls of the guitar.
+
+        Example: 'x2 Volume knob, x1 Tone knob, x1 Three way blade.'
+        """
+        controls_dict = self._get_controls_dict()
+        return ', '.join(
+            f'x{k} {v}' for k, v in controls_dict.items()
+        )
+
+    def _get_controls_dict(self) -> dict[str, int]:
+        controls_str = [control.value for control in self.controls]
+        controls_dict = {}
+        for control in controls_str:
+            try:
+                controls_dict[control] += 1
+            except KeyError:
+                controls_dict[control] = 1
+        return controls_dict
