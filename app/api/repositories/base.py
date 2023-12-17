f9ede06cc3c169060b18d656c80cf1ab2d9f0787
gonza56d <gonza56d@gmail.com>
2023-12-17 17:17:22 -0300

    Inherit connector

--- app/api/repositories/base.py
+++ app/api/repositories/base.py
@@ -1,16 +1,27 @@
 from dataclasses import dataclass
 
-from sqlalchemy import CursorResult, Engine
+from sqlalchemy import CursorResult, Engine, select, UpdateBase
+
+from app.core.repositories.connectors import Connector
 
 
 @dataclass(kw_only=True)
-class SQLRepository:
+class SQLRepository(Connector):
 
     engine: Engine
 
-    def _execute(self, stmt, returning: list | None = None) -> CursorResult:
-        stmt.compile()
+    def _execute(
+            self,
+            stmt: UpdateBase | str,
+            returning: list | None = None
+    ) -> CursorResult:
+        if not isinstance(stmt, str):
+            stmt.compile()
         with self.engine.connect() as conn:
             result = conn.execute(stmt)
             conn.commit()
         return result
+
+    def get_status(self) -> bool | str:
+        """Get if connection is ok and implementation."""
+        result = self._execute('SELECT 1')
