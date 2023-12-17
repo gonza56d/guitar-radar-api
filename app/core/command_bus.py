dcdfa872732706b9a58265c3b55e2c5970e8517f
gonza56d <gonza56d@gmail.com>
2023-12-17 17:17:15 -0300

    Health in command bus

--- app/core/command_bus.py
+++ app/core/command_bus.py
@@ -1,7 +1,11 @@
 from pymessagebus import CommandBus
 
+from app.core.commands.health import GetHealthCommand
 from app.core.commands.bridges import CreateBridgeCommand
 from app.core.handlers.bridges import CreateBridgeHandler
+from app.core.handlers.health import GetHealthHandler
+
 
 command_bus = CommandBus()
 command_bus.add_handler(CreateBridgeCommand, CreateBridgeHandler)
+command_bus.add_handler(GetHealthCommand, GetHealthHandler)
