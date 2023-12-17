c3329f0cccd0e33eeeaccf0bb4fde3fa2b0c6579
gonza56d <gonza56d@gmail.com>
2023-12-17 17:18:07 -0300

    Include health router

--- app/api/main.py
+++ app/api/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 
-from .routers.hello_world import router as hello_world_router
 from .routers.components import router as components_router
+from .routers.health import router as health_router
 
 
 api = FastAPI()
-api.include_router(hello_world_router)
+api.include_router(health_router)
 api.include_router(components_router)
