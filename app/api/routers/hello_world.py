169b22f49c350628f8ca350c0142d74cd1759394
gonza56d <gonza56d@gmail.com>
2023-12-17 16:48:58 -0300

    Remove hello world router

--- app/api/routers/hello_world.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-
-router = APIRouter(prefix='/hello_world', tags=['hello_world'])
-
-
-@router.get('')
-async def get_hello_world():
-    return {'message': 'Hello world'}
