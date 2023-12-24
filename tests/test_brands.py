50e9ec76424fbf335e14ded07dc6e69724052a71
gonza56d <gonza56d@gmail.com>
2023-12-24 17:03:46 -0300

    TDD get brand by id

--- tests/test_brands.py
+++ tests/test_brands.py
@@ -29,3 +29,13 @@ class TestBrands(APITest):
         self.client.post(self.domain_prefix, json={'name': 'schecter', 'founded_in': 1976})
         response = self.client.post(self.domain_prefix, json={'name': 'schecter'})
         assert response.status_code == 422
+
+    def test_get_brand(self):
+        created_response = self.client.post(self.domain_prefix, json={'name': 'jackson', 'founded_in': 1980})
+        created_id = created_response.json().get('id')
+        response = self.client.get(f'{self.domain_prefix}/{created_id}')
+        assert response.json() == {
+            'id': created_id,
+            'name': 'jackson',
+            'founded_in': 1980
+        }
