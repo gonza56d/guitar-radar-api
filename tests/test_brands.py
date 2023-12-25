4f16d900807f44ff9d75c9415c632d92ac617cb3
gonza56d <gonza56d@gmail.com>
2023-12-25 12:02:15 -0300

    Parametrize to test get by name

--- tests/test_brands.py
+++ tests/test_brands.py
@@ -1,6 +1,7 @@
 from uuid import UUID, uuid4
 
-from app.api.exceptions import AlreadyExistsAPIException
+import pytest
+
 from tests.base import APITest
 
 
@@ -30,20 +31,18 @@ class TestBrands(APITest):
         response = self.client.post(self.domain_prefix, json={'name': 'schecter'})
         assert response.status_code == 422
 
-    def test_get_brand(self):
+    @pytest.mark.parametrize('get_by', ['id', 'name'])
+    def test_get_brand(self, get_by: str):
         created_response = self.client.post(self.domain_prefix, json={'name': 'jackson', 'founded_in': 1980})
-        created_id = created_response.json().get('id')
-        response = self.client.get(f'{self.domain_prefix}/{created_id}')
-        assert response.json() == {
-            'id': created_id,
-            'name': 'jackson',
-            'founded_in': 1980
-        }
+        value_to_get_by = created_response.json().get(get_by)
+        response = self.client.get(f'{self.domain_prefix}/{value_to_get_by}')
+        assert response.json() == created_response.json()
 
-    def test_get_brand_not_found(self):
-        id = uuid4()
-        response = self.client.get(f'{self.domain_prefix}/{id}')
+    @pytest.mark.parametrize('get_by', ['id', 'name'])
+    def test_get_brand_not_found(self, get_by: str):
+        get_by_value = uuid4() if get_by == 'id' else 'fender'
+        response = self.client.get(f'{self.domain_prefix}/{get_by_value}')
         assert response.status_code == 404
         assert response.json() == {
-            'message': f'Brand by id {id} was not found.'
+            'message': f'Brand by {get_by} {get_by_value} was not found.'
         }
