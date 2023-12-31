0c77a27f50e9597cf471283561a42c79b7acf410
gonza56d <gonza56d@gmail.com>
2023-12-31 00:17:06 -0300

    Container for auth and session

--- app/containers.py
+++ app/containers.py
@@ -2,13 +2,22 @@ from dependency_injector.containers import DeclarativeContainer, WiringConfigura
 from dependency_injector.providers import Configuration, Factory, Singleton
 from pymessagebus import CommandBus
 from pymongo import MongoClient
+from redis import Redis
 from sqlalchemy import create_engine, Engine
 
+from app.api.repositories.auth import AuthSQLRepository
 from app.api.repositories.brands import BrandSQLRepository
+from app.api.repositories.session import SessionTokenRedisRepository
+from app.api.repositories.users import UserSQLRepository
+from app.core.commands.auth import AuthenticateCommand
 from app.core.commands.brands import GetBrandCommand
+from app.core.handlers.auth import AuthenticateHandler
 from app.core.handlers.brands import CreateBrandHandler, GetBrandHandler
 from app.core.handlers.bridges import CreateBridgeHandler
+from app.core.repositories.auth import AuthRepository
 from app.core.repositories.brands import BrandRepository
+from app.core.repositories.session import SessionRepository
+from app.core.repositories.users import UserRepository
 from app.env import Env
 from app.api.repositories.health import HealthSQLRepository, HealthMongoRepository
 from app.api.repositories.bridges import BridgeSQLRepository
@@ -35,10 +44,20 @@ class Container(DeclarativeContainer):
         create_engine,
         url=sql_db_url
     )
-    mongo_client: Singleton[MongoClient] = Factory(
+    mongo_client: Singleton[MongoClient] = Singleton(
         MongoClient,
         host=Env.DOCUMENT_HOST,
         port=Env.DOCUMENT_PORT
+    )
+    redis_client: Singleton[Redis] = Singleton(
+        Redis,
+        host=Env.CACHE_HOST,
+        port=Env.CACHE_PORT
+    )
+
+    auth_repository: Factory[AuthRepository] = Factory(
+        AuthSQLRepository,
+        engine=sql_engine
     )
 
     brand_repository: Factory[BrandRepository] = Factory(
@@ -63,9 +82,26 @@ class Container(DeclarativeContainer):
         )
     ]
 
+    session_repository: Factory[SessionRepository] = Factory(
+        SessionTokenRedisRepository,
+        secret_key=Env.CACHE_SESSION_SECRET_KEY,
+        token_expiration_minutes=Env.CACHE_TOKEN_EXPIRATION_MINUTES
+    )
+
+    user_repository: Factory[UserRepository] = Factory(
+        UserSQLRepository,
+        engine=sql_engine
+    )
+
     command_bus: Factory[CommandBus] = Factory(
         APICommandBus,
         handlers={
+            AuthenticateCommand: Factory(
+                AuthenticateHandler,
+                auth_repository=auth_repository,
+                user_repository=user_repository,
+                session_repository=session_repository
+            ),
             GetHealthCommand: Factory(
                 GetHealthHandler,
                 *health_repositories
