a298c2fef9556bb01ed03f6c5f1428ad463806bd
gonza56d <gonza56d@gmail.com>
2023-12-31 17:05:54 -0300

    Test session invalid credentials

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -2,6 +2,7 @@ from datetime import date
 from uuid import UUID
 
 import jwt
+import pytest
 
 from app.api.orm.mappings import auth_table, users_table
 from app.core.commands.auth import CreateAuthCommand
@@ -21,13 +22,7 @@ class TestAuth(APITest):
         self.password = 'api_test_password'
         self.users_sql_utils = SQLUtils(users_table)
         self.auth_sql_utils = SQLUtils(auth_table)
-
-    @property
-    def domain_prefix(self) -> str:
-        return '/auth'
-
-    def test_authenticate(self):
-        user = self.users_sql_utils._insert(
+        self.user: User = self.users_sql_utils._insert(
             CreateUserCommand(
                 first_name='John',
                 last_name='Rambo',
@@ -38,16 +33,21 @@ class TestAuth(APITest):
         )
         self.auth_sql_utils._insert(
             CreateAuthCommand(
-                user_id=user.id,
+                user_id=self.user.id,
                 password=hash_password(self.password)
             ),
             Auth
         )
 
+    @property
+    def domain_prefix(self) -> str:
+        return '/auth'
+
+    def test_authenticate(self):
         response = self.client.post(
             self.domain_prefix,
             json={
-                'email': user.email,
+                'email': self.user.email,
                 'password': self.password
             }
         )
@@ -55,4 +55,20 @@ class TestAuth(APITest):
         assert response.status_code == 201
         jwt_token = response.json()['access_token']
         decoded_token = jwt.decode(jwt_token, Env.CACHE_SESSION_SECRET_KEY, algorithms=['HS256'])
-        assert UUID(decoded_token['user_id']) == user.id
+        assert UUID(decoded_token['user_id']) == self.user.id
+
+    @pytest.mark.parametrize(
+        'correct_email, correct_password',
+        [(True, False), (False, True), (False, False)]
+    )
+    def test_authenticate_wrong_credentials(self, correct_email: bool, correct_password: bool):
+        response = self.client.post(
+            self.domain_prefix,
+            json={
+                'email': self.user.email if correct_email else 'wrong@email.com',
+                'password': self.password if correct_password else 'wrong_password'
+            }
+        )
+
+        assert response.status_code == 401
+        assert response.json() == {'message': 'Invalid email or password.'}
